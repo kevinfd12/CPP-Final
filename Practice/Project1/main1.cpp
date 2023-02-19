15e242baf12c4fdb0aa8d0c3476f1d607ff79a18
Kevin Diaz <42587568+kevinfd12@users.noreply.github.com>
2023-02-18 23:24:25 -0500

    Create main1.cpp

---
+++ Practice/Project1/main1.cpp
@@ -0,0 +1,23 @@
+/**
+* myid: [43824] 
+* Those are the last five digits
+* Author: <kevin.diazgochez@baruchmail.cuny.edu>
+* Sample code to help you get started...
+*/
+
+// Kevin F - Project 1 program. Due 2/13/2020 before class
+#include<iostream>
+using namespace std;
+int main() {
+	// define value of alphabets that I need
+	int a = 1, b = 2, c = 3, d = 4, e = 0, f = 0, g = 7, h = 8, i = 0, j = 10, k = 0, l = 12, m = 13, n = 0, o = 15, p = 16, q = 17, r = 18, s = 19, t = 20, u = 21, v = 0, w = 23, x = 24, y = 25, z = 26;
+
+	// find the value of 'a cat'
+	int value = l + i + k + e + n + i + n + g + h + i + s + m + a + k + e + r + t + o + t + h + e + g + r + a + z + e + d + o + x + j + e + h + o + v + a + h + w + h + o + i + n + o + n + e + n + i + g + h + t + w + h + e + n + h + e + p + a + s + s + e + d + f + r + o + m + e + g + y + p + t + m + a + r + c + h + i + n + g + e + q + u + a + l + l + e + d + w + i + t + h + o + n + e + s + t + r + o + k + e + b + o + t + h + h + e + r + f + i + r + s + t + b + o + r + n + a + n + d + a + l + l + h + e + r + b + l + e + a + t + i + n + g + g + o + d + s;
+	// print the value
+	cout << value << endl;
+	//Printed out 1147
+
+	return 0;
+}
+  
